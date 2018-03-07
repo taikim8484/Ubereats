29e7ea9d87c96f307b8d00bd585e5d22a4ee560d
khtai <primnumbe@gmail.com>
2018-03-07 23:40:38 +0700

    #Completed Sign-in / Sign-out with django auth

--- foodtasker/urls.py
+++ foodtasker/urls.py
@@ -1,23 +1,16 @@
-"""foodtasker URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
 from foodtaskerapp import views
-
+from django.contrib.auth import views as auth_views
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('admin/', admin.site.urls),
+    url(r'^$', views.home, name='home'),
+    url(r'^admin/', admin.site.urls),
+    url(r'^restaurant/sign-in/$',auth_views.login,
+        {'template_name' : 'restaurant/sign_in.html'},
+        name = 'restaurant-sign-in'),
+    url(r'^restaurant/sign-out',auth_views.logout,
+        {'next_page' : '/restaurant/sign-in/'},
+        name = 'restaurant-sign-out'),
+    url(r'^restaurant/$',views.restaurant_home,name = "restaurant-home")
 ]
