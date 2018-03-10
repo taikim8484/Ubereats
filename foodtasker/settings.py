51d9b8d494a4607901791fb52cea1281baf9c351
khtai <primnumbe@gmail.com>
2018-03-10 13:36:56 +0700

    Config deployment variable env

--- foodtasker/settings.py
+++ foodtasker/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'rlq%bh01d(s(k&xx+3@p^oag@0govdc)c#4$!*mt)=i6g-eyra'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -75,11 +75,16 @@ WSGI_APPLICATION = 'foodtasker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
 
 
@@ -120,6 +125,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR,'static')
+)
+
 LOGIN_REDIRECT_URL = '/restaurant/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
--- foodtasker/wsgi.py
+++ foodtasker/wsgi.py
@@ -14,3 +14,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "foodtasker.settings")
 
 application = get_wsgi_application()
+
+#Use this to serve static file on heroku
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(application)
