6ece41201f9e2d592b7bbd5abd584eb96a4b6061
khtai <primnumbe@gmail.com>
2018-03-10 11:38:06 +0700

    #Completed Sign-Up function

--- foodtasker/urls.py
+++ foodtasker/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.conf.urls import url
 from foodtaskerapp import views
 from django.contrib.auth import views as auth_views
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^admin/', admin.site.urls),
@@ -14,4 +17,4 @@ urlpatterns = [
      url(r'^restaurant/sign-up',views.restaurant_sign_up,
         name = 'restaurant-sign-up'),
     url(r'^restaurant/$',views.restaurant_home,name = "restaurant-home")
-]
+] + static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
--- foodtaskerapp/forms.py
+++ foodtaskerapp/forms.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import User
 from foodtaskerapp.models import Restaurant
 
 class UserForm(forms.ModelForm):
+    email = forms.CharField(max_length = 100, required = True)
+    password = forms.CharField(widget = forms.PasswordInput())
     class Meta:
         model = User
         fields = { "username", "password", "first_name", "last_name", "email"}
